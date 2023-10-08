5f9b84e8ec4df81afbba30390d4e8fa881ae1bcb
Me <park.pavel04@gmail.com>
2023-10-09 01:58:55 +0600

    Класс Latte, который является конкретным подклассом StarbucksCoffee Реализация абстрактного метода cost()

--- src/Frappuccino.java
+++ src/Frappuccino.java
@@ -2,8 +2,16 @@ class Frappuccino extends StrarbucksCoffee{
     public Frappuccino(){
         description = "Фраппуччино";
     }
-
     public double cost(){
         return 3.00;
+    }
+    class Latte extends StrarbucksCoffee{
+        public Latte(){
+            description = "Латте";
+        }
+
+        public double cost(){
+            return 1.99;
+        }
     }
 }
