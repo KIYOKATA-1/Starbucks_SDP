901a36a27e6f470f0d6ecc8263d4de7aef3bf556
Me <park.pavel04@gmail.com>
2023-10-09 13:17:35 +0600

    Считываем выбор пользователя

--- src/CoffeeShop.java
+++ src/CoffeeShop.java
@@ -13,6 +13,24 @@ public class CoffeeShop {
         System.out.println("3. Мока");
 
 
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                coffee = new Frappuccino();
+                break;
+
+            case 2:
+                coffee = new Latte();
+                break;
+
+            case 3:
+                coffee = new Moka();
+                break;
+
+            default:
+                System.out.println("Нет Такого!!");
+                return;
+        }
 
 
     }
--- src/Frappuccino.java
+++ src/Frappuccino.java
@@ -1,10 +1,12 @@
-class Frappuccino extends StrarbucksCoffee{
-    public Frappuccino(){
+class Frappuccino extends StrarbucksCoffee {
+    public Frappuccino() {
         description = "Фраппуччино";
     }
-    public double cost(){
+
+    public double cost() {
         return 3.00;
     }
+}
     class Latte extends StrarbucksCoffee{
         public Latte(){
             description = "Латте";
@@ -24,4 +26,3 @@ class Frappuccino extends StrarbucksCoffee{
             return 5.99;
         }
     }
-}
